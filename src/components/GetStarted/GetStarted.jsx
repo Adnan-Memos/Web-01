d2a20e3c1210ebce492a5b36be852b1b60858f7d
Adnan Siddiqui <adnansmemail@gmail.com>
2024-01-07 19:53:03 +0530

    Frontend completed for Real Estate Website

--- src/components/GetStarted/GetStarted.jsx
+++ src/components/GetStarted/GetStarted.jsx
@@ -8,10 +8,11 @@ const GetStarted = () => {
         <div className="flexColCenter inner-container">
           <span className="primaryText">Get Started with Homyz</span>
           <span className="secondaryText">
-            Subscribe and find super attractive price quotes from
+            Subscribe and find super attractive price quotes from us.
             <br />
+            Find your residence soon
           </span>
-          <button className="button">
+          <button className="button" href>
             <a href="mailto:adnansmemail@gmail.com">Get Started</a>
           </button>
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import './Header.css'
+import { BiMenuAltRight } from 'react-icons/bi';
+import OutsideClickHandler from 'react-outside-click-handler';
+import{getMenuStyles} from '../../utils/common.js'
 
 const Header = () => {
+  const [menuOpened, setMenuOpened] = React.useState(false);
   return (
       <section className="h-wrapper">
         <div className="flexCenter paddings innerWidth h-container">
           <img src="./logo.png" alt="logo" width={100} />
 
-          <div className="flexCenter h-menu">
-            <a href="">Residencies</a>
-            <a href="">Our Values</a>
-            <a href="">Contact Us</a>
-            <a href="">Get Started</a>
+          <OutsideClickHandler onOutsideClick={()=>setMenuOpened(false)}>
+
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+            <a href="#residencies">Residencies</a>
+            <a href="#value">Our Values</a>
+            <a href="#contact-us">Contact Us</a>
+            <a href="#get-started">Get Started</a>
             <button className='button'>
               <a href="">Contact</a>
             </button>
           </div>
-
+          </OutsideClickHandler>
+          <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
+            <BiMenuAltRight size={30} /> 
+          </div>
         </div>
       </section>
   );
